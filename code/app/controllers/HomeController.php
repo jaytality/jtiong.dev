081fcd3c5ea17dc8706b0821e463c109828b6ee4
jaytality <johnathan.tiong@gmail.com>
2022-08-08 15:21:24 +1000

    Home controller and model

---
+++ code/app/controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+// Controller: Home
+
+namespace spark\Controllers;
+
+use \spark\Core\Controller as Controller;
+
+use \spark\Models\HomeModel;
+
+use \R as R;
+
+class HomeController extends Controller
+{
+    function index()
+    {
+        // $this->viewData['posts'] = BlogModel::getPosts(3);
+        // $this->viewData['servers'] = GameModel::getServersInfo();
+
+		$this->viewOpts['page']['layout']  = 'default';
+        $this->viewOpts['page']['content'] = 'home/index';
+        $this->viewOpts['page']['section'] = 'home';
+        $this->viewOpts['page']['title']   = 'Home';
+
+        $this->view->load($this->viewOpts, $this->viewData);
+    }
+}
+
---
+++ code/app/models/HomeModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spark\Models;
+
+use \spark\Core\Model as Model;
+
+use \R as R;
+
+class HomeModel extends Model
+{
+    //
+}
+
+// end of file
