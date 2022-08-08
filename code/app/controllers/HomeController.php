a367c650687061e268040917fc82b67e4fada3f4
jaytality <johnathan.tiong@gmail.com>
2022-08-08 19:12:50 +1000

    removed commtis data from HomeController

--- code/app/controllers/HomeController.php
+++ code/app/controllers/HomeController.php
@@ -18,32 +18,6 @@ class HomeController extends Controller
     {
         $this->viewData['commits'] = [];
 
-        $curl = new Curl;
-        $projects = $curl->get('https://gitlab.jtiong.dev/api/v4/projects?private_token=' . getConfig('gitlab.token'));
-        $projects = json_decode($projects, true);
-
-        // for each project...
-        foreach ($projects as $project) {
-            $branches = $curl->get('https://gitlab.jtiong.dev/api/v4/projects/' . $project['id'] . '/repository/branches?private_token=' . getConfig('gitlab.token'));
-            $branches = json_decode($branches, true);
-
-            foreach ($branches as $branch) {
-                $commits = $curl->get('https://gitlab.jtiong.dev/api/v4/projects/' . $project['id'] . '/repository/commits?private_token=' . getConfig('gitlab.token'));
-                $commits = json_decode($commits, true);
-
-                foreach ($commits as $commit) {
-                    array_push($this->viewData['commits'], [
-                        'project' => $project['path'],
-                        'branch' => $branch['name'],
-                        'commit_id' => $commit['id'],
-                        'commit_title' => $commit['title'],
-                        'commit_created' => strtotime($commit['created_at'])
-                    ]);
-                }
-            }
-        }
-
-
 		$this->viewOpts['page']['layout']  = 'default';
         $this->viewOpts['page']['content'] = 'home/index';
         $this->viewOpts['page']['section'] = 'home';
