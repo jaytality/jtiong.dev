bc6c5bb5b7494b88d491a3d935f1afba230031ea
jaytality <johnathan.tiong@gmail.com>
2022-08-09 13:15:32 +1000

    splitting pagination buttons out into separate pagination component

--- code/app/views/content/home/index.php
+++ code/app/views/content/home/index.php
@@ -17,49 +17,9 @@
     </div>
 
     <!-- commit messages -->
-    <div class="row">
-        <div class="col-md-9">
-            <!-- page button navigation -->
-            <a href="/" class="btn btn-sm btn-danger<?=$page == 0 ? ' btn-light' : ''?>">&nbsp;&nbsp;1&nbsp;&nbsp;</a>
-            <?php
-                if ($page > 4) {
-                    ?>
-                        <button class="btn btn-sm btn-danger" disabled>&nbsp;&nbsp;...&nbsp;&nbsp;</button>
-                    <?php
-                }
-                // if there's more than 5 pages
-                if ($end > 5) {
-                    for ($i = $from; $i <= $to; $i++) {
-                        if ($from <= 1) {
-                            continue;
-                        } else {
-                            ?>
-                                <a href="<?=($i == 1) ? '/' : '/' . $i?>" class="btn btn-sm btn-danger<?=$page == $i ? ' btn-light' : ''?>"><?='&nbsp;&nbsp;' . $i . '&nbsp;&nbsp;'?></a>
-                            <?php
-                        }
-                    }
-
-                    if ($to != $end) {
-                        ?>
-                            <button class="btn btn-sm btn-danger" disabled>&nbsp;&nbsp;...&nbsp;&nbsp;</button>
-                        <?php
-                    }
-                }
-
-
-                if ($to != $end) {
-                    ?>
-                        <a href="/<?=$end?>" class="btn btn-sm btn-danger<?=$page == $end ? ' btn-light' : ''?>"><?='&nbsp;&nbsp;' . $end . '&nbsp;&nbsp;'?></a>
-                    <?php
-                }
-            ?>
-            <br />
-            <br />
-        </div>
-        <div class="col-md-3">
-            <!-- prev/next navigation -->
-        </div>
-    </div>
+    <?php
+        include(views . '/_components/pagination.php');
+    ?>
     <div class="row">
         <div class="col-md-12">
             <table class="table">
@@ -89,15 +49,7 @@
             </table>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <?php
-                for ($i = 1; $i <= $end; $i++) {
-                    ?>
-                        <a href="<?=($i == 1) ? '/' : '/' . $i?>" class="btn btn-danger<?=$page == $i ? ' btn-light' : ''?>"><?=$i?></a>
-                    <?php
-                }
-            ?>
-        </div>
-    </div>
+    <?php
+        include(views . '/_components/pagination.php');
+    ?>
 </div>
