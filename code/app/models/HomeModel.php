9bef9a420aa061e69449ada006961a73bd148494
jaytality <johnathan.tiong@gmail.com>
2022-08-09 14:10:59 +1000

    getOldestCommit and getNewestCommit methods added

--- code/app/models/HomeModel.php
+++ code/app/models/HomeModel.php
@@ -8,7 +8,15 @@ use \R as R;
 
 class HomeModel extends Model
 {
-    //
+    public function getOldestCommit()
+    {
+        return R::findOne('commits', ' ORDER BY time ASC LIMIT 1');
+    }
+
+    public function getNewestCommit()
+    {
+        return R::findOne('commits', ' ORDER BY time DESC LIMIT 1');
+    }
 }
 
 // end of file
