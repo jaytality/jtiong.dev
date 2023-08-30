df0ce65c797f48426d1f3f019a06479cd1bd1847
jaytality <johnathan.tiong@gmail.com>
2023-08-30 20:38:12 +1000

    initial commit

--- stop_production.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cd docker && docker compose -f docker-compose.production.yml down
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '11d42b7ccac539e2374942d3a3798699fc591255',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '11d42b7ccac539e2374942d3a3798699fc591255',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
