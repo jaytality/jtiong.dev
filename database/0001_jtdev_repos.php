6a7b040da26ad22b6e1bbf47d4c7b5524cf8aec8
jaytality <johnathan.tiong@gmail.com>
2023-09-01 08:59:57 +1000

    added repos migration

--- database/0001_jtdev_repos.php
+++ database/0001_jtdev_repos.php
@@ -5,10 +5,12 @@ use ILluminate\Database\Capsule\Manager as Capsule;
 if (!Capsule::schema()->hasTable('jtdev_repos')) {
     Capsule::schema()->create('jtdev_repos', function($table) {
         $table->increments('id');
-        $table->string('name');
-        $table->date('date');
-        $table->boolean('visible')->default(true);
-        $table->text('description')->nullable();
+        $table->string('github');                       // github (node_id)
+        $table->string('name');                         // (name)
+        $table->timestamp('created');                   // created timestamp (created_at)
+        $table->boolean('visible')->default(true);      // visible on jtiong.dev (if no, all commits are shielded)
+        $table->text('description')->nullable();        // (description)
+        $table->text('url')->nullable();                // html_url of the repo
     });
 }
 
