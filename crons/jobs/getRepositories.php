209453eac5dfe46c68dbe7dbccc7daeab1cdf15c
jaytality <johnathan.tiong@gmail.com>
2023-09-20 17:39:08 +1000

    updated query and response handling

--- crons/jobs/getRepositories.php
+++ crons/jobs/getRepositories.php
@@ -15,31 +15,43 @@ require_once '../../database.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-$accessToken    = getConfig('github.token');
-$githubUsername = getConfig('github.username');
-
 // Function to get the list of repositories for a username
-function getRepositoriesForUser($accessToken, $githubUsername)
+function getRepositoriesForUser($accessToken)
 {
-    $url = "https://api.github.com/user/repos?visibility=all&affiliation=owner&per_page=100&page=1";
+    $params = [
+        'visiblity'   => 'all',
+        'affiliation' => 'owner',
+        'per_page'    => 100,
+        'page'        => 1,
+    ];
+
+    $params = http_build_query($params);
+
+    $url = "https://api.github.com/user/repos?" . $params;
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         "Accept: application/vnd.github+json",
-        "Authorization: token {$accessToken}",
+        "Authorization: Bearer " . $accessToken,
         "X-GitHub-Api-Version: 2022-11-28",
     ]);
 
-    $response = curl_exec($ch);
+    if (curl_error($ch)) {
+        echo "REQUEST ERROR: " . curl_error($ch) . "\n\n";
+        die();
+    } else {
+        $response = curl_exec($ch);
+    }
+
     curl_close($ch);
 
     return json_decode($response, true);
 }
 
 // Get the repositories
-$repositories = getRepositoriesForUser($accessToken, $githubUsername);
+$repositories = getRepositoriesForUser(getConfig('github.token'));
 
 // truncate the table - Github should always be the source of truth
 // by doing this - any deleted repos will also no longer be relevant to the frontend
