eca62691275e4db4427455f0dde1126ab7e4d0d7
jaytality <johnathan.tiong@gmail.com>
2023-09-21 15:38:36 +1000

    made the repo name a link

--- app/views/content/home/index.php
+++ app/views/content/home/index.php
@@ -36,7 +36,7 @@
                                     <tr>
                                         <td>
                                             <span style="font-size: 1.1rem; ">
-                                                <strong class="text-author"><?=$commit->repo_name?></strong><span class="text-project">#<?=substr($commit->sha, 0, 6)?></span> &bull;
+                                                <strong><a href="/project/<?=$commit->repo_name?>" class="text-author"><?=$commit->repo_name?></a></strong><span class="text-project">#<?=substr($commit->sha, 0, 6)?></span> &bull;
                                                 <small class="text-muted"><?=$time->niceOutput(strtotime($commit->date))?></small>
                                             </span>
                                             <p style="color: #ccc; "><?=$commit->message?></p>
