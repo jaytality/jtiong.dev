d9ad484b73f6d126165db9f5826bab108d464377
jaytality <johnathan.tiong@gmail.com>
2023-09-23 17:59:18 +1000

    updated layout

--- app/views/content/home/login.php
+++ app/views/content/home/login.php
@@ -6,11 +6,11 @@
             <form action="/" method="POST">
                 <div class="form-group row">
                     <label for="email" class="col-md-3 text-right">Email Address</label>
-                    <input type="email" class="form-control col-md-9" id="email">
+                    <input type="email" class="form-control col-md-6" id="email">
                 </div>
                 <div class="form-group row">
                     <label for="password" class="col-md-3 text-right">Password</label>
-                    <input type="password" class="form-control col-md-9" id="password">
+                    <input type="password" class="form-control col-md-6" id="password">
                 </div>
                 <div class="form-group row">
                     <div class="col-md-3 offset-md-3">
